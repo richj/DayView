e61de487dae5665cdec58db819fa9d1d047e827e
richj <rich@unami.com>
2025-06-07 21:45:51 -0600

    first

--- src/App.vue
+++ src/App.vue
@@ -4,44 +4,98 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container-fluid py-5">
+    <div class="row g-4">
+      <!-- First major column -->
+      <div class="col-12 col-lg-6">
+        <div class="row g-4">
+          <div class="col-12 col-md-6 d-flex align-items-start justify-content-center">
+            <div class="w-100" style="min-width:320px;">
+              <h5 class="mb-3 text-center">ABCDEFG</h5>
+              <ul class="list-unstyled text-center">
+                <li>Awake\Bed</li>
+                <li>Coffee</li>
+                <li>Start Journal</li>
+                <li>Dailies</li>
+                <li>Litter/Laundry</li>
+                <li>Dishes</li>
+                <li>Trash</li>
+                <li>Surfaces</li>
+                <li>Floors</li>
+                <li>Area</li>
+                <li>Energy</li>
+                <li>Freshen</li>
+                <li>Get things done</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 d-flex align-items-start justify-content-center">
+            <div class="w-100 text-center" style="min-width:320px;">
+              <h5 class="mb-3 text-center">Daily Dozen</h5>
+              <ul class="list-unstyled text-center">
+                <li>Sleep</li>
+                <li>Exercise</li>
+                <li>Fuel</li>
+                <li>Meditate</li>
+                <li>System</li>
+                <li>People</li>
+                <li>Act</li>
+                <li>Light</li>
+                <li>Mastery</li>
+                <li>Eternal</li>
+                <li>Routine</li>
+                <li>Programming</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Second major column -->
+      <div class="col-12 col-lg-6">
+        <div class="row g-4">
+          <div class="col-12 col-md-6 d-flex align-items-start justify-content-center">
+            <div class="w-100 text-center" style="min-width:320px;">
+              <h5 class="mb-3">DO</h5>
+              <ul class="list-unstyled text-center">
+                <li>Walk</li>
+                <li>Sit</li>
+                <li>Listen</li>
+                <li>Create</li>
+                <li>Release</li>
+                <li>Be</li>
+                <li>Love</li>
+                <li>Record</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 d-flex align-items-start justify-content-center">
+            <div class="w-100 text-center" style="min-width:320px;">
+              <h5 class="mb-3">DON'T</h5>
+              <ul class="list-unstyled text-center">
+                <li>Tank</li>
+                <li>Terrorize</li>
+                <li>Talk</li>
+                <li>Trash</li>
+                <li>Take</li>
+                <li>Tune out</li>
+                <li>Trespass</li>
+                <li>Tether</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+html, body, #app {
+  height: 100%;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+body, #app {
+  border: 4px solid #007bff;
+  min-height: 100vh;
+  box-sizing: border-box;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
-
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.provide('bootstrap', bootstrap);
+app.mount('#app');
+// createApp(App).mount('#app')
